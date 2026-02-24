145b62a03479bdf6844f393f9c1ddb06faad14a2
hwenj <146937278+hwenj@users.noreply.github.com>
2026-02-24 19:04:00 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,32 +6,45 @@ import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path='/signup'
-        element={
-          <ProtectedRoute reverseLogic>
-            <SignUp />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/login'
-        element={
-          <ProtectedRoute reverseLogic>
-            <LogIn />
-          </ProtectedRoute>
-        }
-      ></Route>
-      <Route
-        path='/home'
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-      ></Route>
-    </Routes>
+    <>
+      <div
+        style={{
+          padding: '10px',
+          backgroundColor: '#f0f0f0',
+          textAlign: 'center',
+          borderBottom: '1px solid #ccc',
+        }}
+      >
+        <h2>Bienvenido</h2>
+        <p>Prueba de navegación</p>
+      </div>
+      <Routes>
+        <Route
+          path='/signup'
+          element={
+            <ProtectedRoute reverseLogic>
+              <SignUp />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <ProtectedRoute reverseLogic>
+              <LogIn />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route
+          path='/home'
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        ></Route>
+      </Routes>
+    </>
   );
 }
 
